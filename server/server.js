362d3eda3b5313bee925fcf29fcbede001c8175d
Robyn Noble <robynhurd09@gmail.com>
2021-05-23 08:26:21 -0500

    app and server files updated

--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const creatureRouter = require('./routes/creature.router');
+
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
-app.use(express.static('build'));
+
 
 /** ---------- TODO - EXPRESS ROUTES ---------- **/
-
+app.use('/creature', creatureRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,32 @@
 import {useState} from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
+import home from '../home';
+import upload from '../upload';
 import './App.css';
 
 function App () {
   
   return (
-    <h1 className="App">
-      Hello! I'm a blank starter App!
-    </h1>
+    <div>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Gallery</Link>
+            </li>
+            <li>
+              <Link to="/upload">Upload</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" component={home} />
+          <Route path="/upload" component={upload} />
+        </Switch>
+      </Router>
+    </div>
   );
-
 }
 
-export default App
+export default App;
